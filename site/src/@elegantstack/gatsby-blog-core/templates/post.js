87bb424d4576722c4c5b53fa1148f07d1e54558f
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-02-03 06:15:59 +0400

    Implement language switch using counterpart_path from frontmatter

---
+++ site/src/@elegantstack/gatsby-blog-core/templates/post.js
@@ -0,0 +1,85 @@
+import { graphql } from 'gatsby'
+import PostPage from '../containers/Post'
+
+export default PostPage
+
+export const pageQuery = graphql`
+  query PostPageQuery(
+    $id: String!
+    $language: String! = "en"
+    $previousId: String
+    $nextId: String
+    $categoryId: String
+    $tagsIds: [String]
+    $hasTags: Boolean!
+    $includeExcerpt: Boolean!
+    $includeTimeToRead: Boolean!
+    $includeTableOfContents: Boolean!
+    $imageQuality: Int!
+  ) {
+    post: article(id: { eq: $id }) {
+      ...ArticleInformation
+      ...ArticleThumbnailRegular
+      counterpart_path
+    }
+    tagCategoryPosts: allArticle(
+      filter: {
+        private: { ne: true }
+        draft: { ne: true }
+        tags: { elemMatch: { id: { in: $tagsIds } } }
+        category: { id: { eq: $categoryId } }
+        language: { eq: $language }
+        id: { ne: $id }
+      }
+      sort: { date: ASC }
+      limit: 6
+    ) @include(if: $hasTags) {
+      nodes {
+        ...ArticlePreview
+        ...ArticleThumbnailRegular
+      }
+    }
+    tagPosts: allArticle(
+      filter: {
+        private: { ne: true }
+        draft: { ne: true }
+        tags: { elemMatch: { id: { in: $tagsIds } } }
+        id: { ne: $id }
+        language: { eq: $language }
+      }
+      sort: { date: ASC }
+      limit: 6
+    ) @include(if: $hasTags) {
+      nodes {
+        ...ArticlePreview
+        ...ArticleThumbnailRegular
+      }
+    }
+    categoryPosts: allArticle(
+      filter: {
+        private: { ne: true }
+        draft: { ne: true }
+        category: { id: { eq: $categoryId } }
+        language: { eq: $language }
+        id: { ne: $id }
+      }
+      sort: { date: ASC }
+      limit: 6
+    ) {
+      nodes {
+        ...ArticlePreview
+        ...ArticleThumbnailRegular
+      }
+    }
+    previous: article(id: { eq: $previousId }, language: { eq: $language }) {
+      id
+      slug
+      title
+    }
+    next: article(id: { eq: $nextId }, language: { eq: $language }) {
+      id
+      slug
+      title
+    }
+  }
+`
