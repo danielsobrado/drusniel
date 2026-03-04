70a6da24282c5f891d573dd5b4397dd168cc1335
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-03-04 15:42:47 +0400

    new images

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -4,7 +4,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       module: {
         rules: [
           {
-            test: /(react-share|algoliasearch|@algolia\/.*)/,
+            test: /react-slick|slick-carousel|enquire\.js|whatwg-url/,
             use: loaders.null()
           }
         ]
