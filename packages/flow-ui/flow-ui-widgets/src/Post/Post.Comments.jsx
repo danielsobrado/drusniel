a6845f4b6638eb7db585a27248e3338878265d72
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-03-04 16:36:35 +0400

    fix

--- packages/flow-ui/flow-ui-widgets/src/Post/Post.Comments.jsx
+++ packages/flow-ui/flow-ui-widgets/src/Post/Post.Comments.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Box, Divider } from 'theme-ui'
-import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
 
 const PostComments = ({ title, id }) => {
+  if (typeof window === 'undefined') return null
+
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { Disqus, CommentCount } = require('gatsby-plugin-disqus')
+
   let disqusConfig = {
     identifier: id,
     title: title
